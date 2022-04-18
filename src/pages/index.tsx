efda93fcf248e4e7348ec835baf2e03d88995846
Axelvernik <vernik.pl@gmail.com>
2022-04-18 16:08:08 +0300

    3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>BugBearz Treasury</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="BugBearz Treasury"
         />
       </Head>
       <HomeView />

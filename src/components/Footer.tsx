73ef0d06b6a62da9ac0bbc240b5e170d1d303e4b
Axelvernik <vernik.pl@gmail.com>
2022-04-18 19:59:38 +0300

    78

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: FC = () => {
     return (
         <div className="flex-none px-0 pt-4">
             <footer className="items-center p-4 footer bg-neutral text-neutral-content">
-                <div className="foot"><img src="foot.png" /></div>
+                <div className="foot"><img src="foot2.jpg" /></div>
             </footer>
         </div>
     );

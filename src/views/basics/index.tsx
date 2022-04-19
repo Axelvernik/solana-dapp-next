4560e8c5b5cd961d10c59ce9cdd4513c806fc321
Axelvernik <vernik.pl@gmail.com>
2022-04-19 13:06:56 +0300

    444

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -2,31 +2,28 @@
 import { FC } from "react";
 import { SignMessage } from '../../components/SignMessage';
 import { SendTransaction } from '../../components/SendTransaction';
-import React from "react";
-
-// reactstrap components
-import {Container, Row, Col} from 'react-bootstrap';
 
 export const BasicsView: FC = ({ }) => {
 
   return (
     <div className="hero mx-auto p-4 min-h-16 py-4">
       <div className="hero-content flex flex-col">
-      <h1 className="text-3xl font-bold g">
+      <h1 className="text-3xl font-bold">
         BugBearz Treasury
       </h1>
         {}
- <Row>
-          <Col>
-            <span><img src="gold.gif" /></span>
-          </Col>
-          <Col xs="6">
-            <span><img src="gold.gif" /></span>
-          </Col>
-          <Col>
-            <span><img src="gold.gif" /></span>
-          </Col>
-        </Row>
+        <div className="row">
+   <div className="col">
+     <img src="gold.gif" />
+   </div>
+   <div className="col-6">
+     <img src="gold.gif" />
+   </div>
+   <div className="col">
+     <img src="gold.gif" />
+   </div>
+ </div>
+
         <div className="p-2 text-center">
          <a className="btn btnn btn-sm rounded-btn">Create Staking Pool</a>
           <SignMessage/>

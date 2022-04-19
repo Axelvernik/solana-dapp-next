976e3b41ea8f3bfa7947516916f843e6b90849e9
Axelvernik <vernik.pl@gmail.com>
2022-04-19 09:12:02 +0300

    98

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -8,11 +8,12 @@ export const BasicsView: FC = ({ }) => {
   return (
     <div className="hero mx-auto p-4 min-h-16 py-4">
       <div className="hero-content flex flex-col max-w-lg">
-        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          Basics
-        </h1>
+      <h1 className="text-3xl pl-12 font-bold">
+        BugBearz Treasury
+      </h1>
         {/* CONTENT GOES HERE */}
         <div className="p-2 text-center">
+         <a className="btn btnn btn-sm rounded-btn">Create Staking Pool</a>
           <SignMessage/>
           <SendTransaction />
           {<p>&lt; your content &gt;</p>}

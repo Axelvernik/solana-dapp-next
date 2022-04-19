a6842e74a699b6cd6c828574bea94a6b411a63db
Axelvernik <vernik.pl@gmail.com>
2022-04-19 09:20:08 +0300

    Update index.tsx

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -7,8 +7,8 @@ export const BasicsView: FC = ({ }) => {
 
   return (
     <div className="hero mx-auto p-4 min-h-16 py-4">
-      <div className="hero-content flex flex-col max-w-lg">
-      <h1 className="text-3xl pl-12 font-bold">
+      <div className="hero-content flex flex-col">
+      <h1 className="text-3xl font-bold">
         BugBearz Treasury
       </h1>
         {/* CONTENT GOES HERE */}
